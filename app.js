71cdcb539b0760d9c85c1e88f113258b091da368
Valeria Serci <balerty84@gmail.com>
2021-03-22 19:38:04 +0000

    add if statement to check if the adult is over 18 and registered early

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
-const earlyRunner = true
-const runnerAge = 20
+const earlyRunner = true;
+
+const runnerAge = 20;
+
+if (runnerAge => 18 && earlyRunner === true){ raceNumber += 1000;
+}
