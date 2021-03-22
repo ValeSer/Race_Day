4d53f915fc3145e1cb765a775d2d758059a3cfc6
Valeria Serci <balerty84@gmail.com>
2021-03-22 19:54:41 +0000

    add elsif that prints the later runners time

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
-const earlyRunner = true;
+const earlyRunner = false;
 
 const runnerAge = 20;
 
-if (runnerAge => 18 && earlyRunner === true){ raceNumber += 1000;
+if (runnerAge >= 18 && earlyRunner === true){ raceNumber += 1000;
 }
 
-if (runnerAge => 18 && earlyRunner === true){ 
-    console.log(`You will race at 9:30am with the number ${raceNumber}.`);
+if (runnerAge >= 18 && earlyRunner === true){
+     console.log(`You will race at 9:30am with the number ${raceNumber}.`);
+} else if(runnerAge >= 18 && !earlyRunner === true) {
+  console.log(`You will race at 11:00am with the number ${raceNumber}.`);
 }
+
