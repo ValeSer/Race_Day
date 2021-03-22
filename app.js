389c97b5d0f36f7a5a629823e12cf4d784eae0fb
Valeria Serci <balerty84@gmail.com>
2021-03-22 19:43:25 +0000

    add if statement that prints to the console

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const runnerAge = 20;
 
 if (runnerAge => 18 && earlyRunner === true){ raceNumber += 1000;
 }
+
+if (runnerAge => 18 && earlyRunner === true){ 
+    console.log(`You will race at 9:30am with the number ${raceNumber}.`);
+}
