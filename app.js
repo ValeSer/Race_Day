e715f62183b7f5623bbaa909fc9e0523091851b0
Valeria Serci <balerty84@gmail.com>
2021-03-25 18:28:11 +0000

    managing 18 yo people

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const earlyRunner = false;
 const lateRunner = true;
 const youthRunner = true
 
-const runnerAge = 16;
+const runnerAge = 18;
 
 if (runnerAge >= 18 && earlyRunner === true){ raceNumber += 1000;
 }
 
-if (runnerAge >= 18 && earlyRunner === true){ console.log(`You will race at 9:30 am with the number ${raceNumber}.`);
-} else if(runnerAge >= 18 && !earlyRunner === true) {
+if (runnerAge > 18 && earlyRunner === true){ console.log(`You will race at 9:30 am with the number ${raceNumber}.`);
+} else if(runnerAge > 18 && !earlyRunner === true) {
   console.log(`You will race at 11:00 am with the number ${raceNumber}.`);
 } else if (runnerAge < 18 ) {
   console.log(`You will race at 12:30 pm with the number ${raceNumber}.`)
+} else if (runnerAge === 18 ) {
+  console.log(`You should see the registration desk.`);
 }
 
