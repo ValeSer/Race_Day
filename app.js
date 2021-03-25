c3888d4358f69e7ef2816a974946fc2e87156c92
Valeria Serci <balerty84@gmail.com>
2021-03-25 18:21:44 +0000

    else if statement for youths

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
 const earlyRunner = false;
+const lateRunner = true;
+const youthRunner = true
 
-const runnerAge = 20;
+const runnerAge = 16;
 
 if (runnerAge >= 18 && earlyRunner === true){ raceNumber += 1000;
 }
 
-if (runnerAge >= 18 && earlyRunner === true){
-     console.log(`You will race at 9:30am with the number ${raceNumber}.`);
+if (runnerAge >= 18 && earlyRunner === true){ console.log(`You will race at 9:30 am with the number ${raceNumber}.`);
 } else if(runnerAge >= 18 && !earlyRunner === true) {
-  console.log(`You will race at 11:00am with the number ${raceNumber}.`);
+  console.log(`You will race at 11:00 am with the number ${raceNumber}.`);
+} else if (runnerAge < 18 ) {
+  console.log(`You will race at 12:30 pm with the number ${raceNumber}.`)
 }
 
